10d9949ba1198dd890a511b1dd345a2f01a2d524
manu <manu@gmail.com>
2025-07-20 16:30:12 +0530

    db updated

--- connectdb/db.js
+++ connectdb/db.js
@@ -1,14 +1,22 @@
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize('expenses', 'root', 'Manuraja@767', {
-  host: 'localhost',
-  dialect: 'mysql'
+import dotenv from "dotenv";
+dotenv.config(); 
+
+const database = process.env.MYSQL_DATABASE;
+const username = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOST;
+
+export const sequelize = new Sequelize(database, username, password, {
+  host, 
+  dialect: "mysql",
 });
 
-export const db = async()=>{
-    try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-}
+export const db = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ DB connection established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+  }
+};
